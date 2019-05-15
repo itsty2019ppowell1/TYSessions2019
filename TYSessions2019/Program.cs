96d29a55df86b0ba3a85d3e07089c6d2d75b934d
ITSPaul <powell.paul@itsligo.ie>
2019-05-15 22:02:54 +0100

    Initial Setup

--- TYSessions2019/Program.cs
+++ TYSessions2019/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActivityTracker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,10 @@ namespace TYSessions2019
 {
     class Program
     {
-        static void Main(string[] args)
+         
+    static void Main(string[] args)
         {
+            
         }
     }
 }
